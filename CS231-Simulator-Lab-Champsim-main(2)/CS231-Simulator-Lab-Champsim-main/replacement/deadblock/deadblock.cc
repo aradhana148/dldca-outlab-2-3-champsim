3b09cffaa8d2566b9d49c7bbe0c2ff34c1d1a425
aradhana148 <nira.aradhana7@gmail.com>
2025-11-21 06:02:32 +0530

    task2

--- CS231-Simulator-Lab-Champsim-main(2)/CS231-Simulator-Lab-Champsim-main/replacement/deadblock/deadblock.cc
+++ CS231-Simulator-Lab-Champsim-main(2)/CS231-Simulator-Lab-Champsim-main/replacement/deadblock/deadblock.cc
@@ -10,6 +10,18 @@ void CACHE::initialize_replacement() {}
 // find replacement victim
 uint32_t CACHE::find_victim(uint32_t cpu, uint64_t instr_id, uint32_t set, const BLOCK* current_set, uint64_t ip, uint64_t full_addr, uint32_t type)
 {
+  int dead=-1;
+  for (uint32_t i=0;i<NUM_WAY;i++){
+    if(dp_predictor.predict_to_be_dead(current_set[i].ip)){
+      if(dead==-1 || (current_set[i].lru>current_set[dead].lru)){
+        dead=i;
+      }
+    }
+  }
+  if(dead>-1){
+    return dead;
+  }
+  
   return std::distance(current_set, std::max_element(current_set, std::next(current_set, NUM_WAY), lru_comparator<BLOCK, BLOCK>()));
 }
 
